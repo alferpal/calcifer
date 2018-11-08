a5d9772bf91edcccb85ced8e2584dc1db37c1c86
Alex Ferrando <alferpal@gmail.com>
2018-11-08 20:02:42 +0100

    fix(utils-test): Fix warning handler tests

--- packages/calcifer-utils/test/fixtures/warningHandler.ts
+++ packages/calcifer-utils/test/fixtures/warningHandler.ts
@@ -4,4 +4,6 @@ import { setProcessDefaults } from '../../src/lib/process-defaults'
 
 setProcessDefaults()
 
-process.emitWarning('fixture warning')
+process.emitWarning('fixtureDesignedWarning')
+
+setTimeout(process.exit, 512)
