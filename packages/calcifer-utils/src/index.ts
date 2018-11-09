db574cf60cc36046d0d5a3a7efe8c9ab808e8e7f
Alex Ferrando <alferpal@gmail.com>
2018-11-09 16:01:47 +0100

    feat(utils-routeLoader): Add function to load routes from a folder

--- packages/calcifer-utils/src/index.ts
+++ packages/calcifer-utils/src/index.ts
@@ -1,9 +1,11 @@
 'use strict'
 
+import { getRoutes } from './lib/route-loader'
 import { logger } from './lib/log'
 import { setProcessDefaults } from './lib/process-defaults'
 
 export {
+  getRoutes,
   logger,
   setProcessDefaults,
 }
