862a7a622c48258e9b210b370ce155461aa2e2a4
Alexandre Ferrando <alferpal@gmail.com>
2018-11-09 12:27:44 +0100

    test(utils-processDefaults): Enhance tests for setProcessDefaults

---
+++ packages/calcifer-utils/test/lib/fixtures/unhandled-rejection-handler.ts
@@ -0,0 +1,9 @@
+'use strict'
+
+import { setProcessDefaults } from '../../../src/lib/process-defaults'
+
+setProcessDefaults()
+
+new Promise((_, reject) => {
+  reject(new Error('catch this!'))
+})
