cd340d4af4d380f3bd27699819d00233b6647323
Alex Ferrando <alferpal@gmail.com>
2018-11-09 16:30:38 +0100

    feat(server): PoC of calcifer-server

--- packages/calcifer-server/src/routes/coffee.ts
+++ packages/calcifer-server/src/routes/coffee.ts
@@ -3,22 +3,20 @@
 // Funny / nonsensical route just to test that
 // this API in fact loads multiple routes from multiple files
 
-import { Request, ResponseToolkit } from 'hapi'
-
 import boom from 'boom'
 
 const routes = [
   {
     method: ['GET'],
     path: '/coffee',
-    async handler(request: Request, h: ResponseToolkit) {
+    async handler() {
       return boom.teapot()
     },
   }, {
     method: ['GET'],
     path: '/tea',
-    async handler(request: Request, h: ResponseToolkit) {
-      return boom.teapot(`I'm a server, not a teapot!`)
+    async handler() {
+      return boom.teapot('I\'m a server, not a teapot!')
     },
   },
 ]
