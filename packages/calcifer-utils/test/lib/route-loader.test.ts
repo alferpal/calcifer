e9b6c14da0b40f271a59b6f3add5a404561bcaaa
Alexandre Ferrando <alferpal@gmail.com>
2018-11-12 20:24:15 +0100

    test(utils): Fix test descriptions, incorrect assertion in process-defaults

--- packages/calcifer-utils/test/lib/route-loader.test.ts
+++ packages/calcifer-utils/test/lib/route-loader.test.ts
@@ -4,14 +4,14 @@ import * as innerLib from '../../src/lib/route-loader'
 import * as exported from '../../src'
 import path from 'path'
 
-describe('When good-defaults is required directly:', () => {
+describe('When getRoutes is required directly:', () => {
   test('getRoutes should be defined and be a function', () => {
     expect(innerLib.getRoutes).toBeDefined()
     expect(typeof innerLib.getRoutes).toBe('function')
   })
 })
 
-describe('When good-defaults is required from outside:', () => {
+describe('When getRoutes is required from outside:', () => {
   test('getRoutes should be defined and be a function', () => {
     expect(exported.getRoutes).toBeDefined()
     expect(typeof exported.getRoutes).toBe('function')
