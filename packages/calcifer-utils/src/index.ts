6b948bf16c1e0891dd71e03a185da8687e737e6d
Alexandre Ferrando <alferpal@gmail.com>
2018-11-15 16:47:19 +0100

    feat(utils-testSuprocessHelper): Add helpers for spawning test suprocesses

--- packages/calcifer-utils/src/index.ts
+++ packages/calcifer-utils/src/index.ts
@@ -2,8 +2,16 @@
 
 import { logger } from './lib/log'
 import { setProcessDefaults } from './lib/process-defaults'
+import { execFileHelper, forkHelper, spawnHelper } from './lib/test-subprocess-helper'
+
+const testHelpers = {
+  execFileHelper,
+  forkHelper,
+  spawnHelper,
+}
 
 export {
   logger,
   setProcessDefaults,
+  testHelpers,
 }
