ca630ea6a8989ee86f72b1b361fb79893672a000
Alex Ferrando <alferpal@gmail.com>
2018-12-10 17:32:34 +0100

    chore(project): Add codecov

--- packages/calcifer-utils/src/lib/log.ts
+++ packages/calcifer-utils/src/lib/log.ts
@@ -5,8 +5,8 @@ import pino = require('pino')
 const logger = pino(pino.destination())
 
 const childLogger = logger.child({
-  calciferName: process.env.CALCIFER_NAME || 'ENV.CALCIFER_NAME NOT PRESENT',
-  calciferType: process.env.CALCIFER_TYPE || 'ENV.CALCIFER_TYPE NOT PRESENT',
+  calciferName: process.env.CALCIFER_NAME || 'env.CALCIFER_NAME not present',
+  calciferType: process.env.CALCIFER_TYPE || 'env.CALCIFER_TYPE not present',
 })
 const finalLogger = pino.final(childLogger)
 
