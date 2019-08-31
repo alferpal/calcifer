dd2103c5723d2e140d34af9b91f0114d43fb4f14
Alex Ferrando <alferpal@gmail.com>
2019-08-31 21:05:17 +0200

    style(server-tests): fix style in server tests

--- packages/calcifer-server/test/routes/teapot.test.ts
+++ packages/calcifer-server/test/routes/teapot.test.ts
@@ -3,45 +3,48 @@ import { prepareServer, server } from '../../src/server'
 
 import boom = require('@hapi/boom')
 
-// eslint-disable-next-line jest/no-hooks
-beforeAll(async () => {
-  await prepareServer()
-  await server.initialize()
-})
+describe('teapot tests', () => {
 
-describe('about the /coffee route', () => {
-  it('it should return a 418 error', async () => {
-    expect.assertions(3)
-
-    const options = {
-      method: 'GET',
-      url: '/coffee',
-    }
-
-    const response = await server.inject(options)
-    const payload: boom.Payload = JSON.parse(response.payload)
-
-    expect(payload.statusCode).toStrictEqual(418)
-    expect(payload.message).toStrictEqual('I\'m a teapot')
-    expect(payload.error).toStrictEqual(payload.message)
+  // eslint-disable-next-line jest/no-hooks
+  beforeAll(async () => {
+    await prepareServer()
+    await server.initialize()
   })
-})
 
-describe('about the /tea route', () => {
-  it('it should return a 418 error with a custom message', async () => {
-    expect.assertions(3)
+  describe('about the /coffee route', () => {
+    it('it should return a 418 error', async () => {
+      expect.assertions(3)
 
-    const options = {
-      method: 'GET',
-      url: '/tea',
-    }
+      const options = {
+        method: 'GET',
+        url: '/coffee',
+      }
 
-    const response = await server.inject(options)
+      const response = await server.inject(options)
+      const payload: boom.Payload = JSON.parse(response.payload)
 
-    const payload: boom.Payload = JSON.parse(response.payload)
+      expect(payload.statusCode).toStrictEqual(418)
+      expect(payload.message).toStrictEqual('I\'m a teapot')
+      expect(payload.error).toStrictEqual(payload.message)
+    })
+  })
 
-    expect(payload.statusCode).toStrictEqual(418)
-    expect(payload.message).toStrictEqual('I\'m a server, not a teapot!')
-    expect(payload.error).not.toStrictEqual(payload.message)
+  describe('about the /tea route', () => {
+    it('it should return a 418 error with a custom message', async () => {
+      expect.assertions(3)
+
+      const options = {
+        method: 'GET',
+        url: '/tea',
+      }
+
+      const response = await server.inject(options)
+
+      const payload: boom.Payload = JSON.parse(response.payload)
+
+      expect(payload.statusCode).toStrictEqual(418)
+      expect(payload.message).toStrictEqual('I\'m a server, not a teapot!')
+      expect(payload.error).not.toStrictEqual(payload.message)
+    })
   })
 })
