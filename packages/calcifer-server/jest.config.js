8a87b5df7da848d67437119de0f415d91375a826
Alex Ferrando <alferpal@gmail.com>
2019-10-23 14:44:06 +0200

    chore(project): add displayName and name to jest configs

--- packages/calcifer-server/jest.config.js
+++ packages/calcifer-server/jest.config.js
@@ -1,16 +1,18 @@
-'use strict'
 
+const { name } = require('./package')
 const jestBase = require('../../jest.config.js')
 
 const config = {
-  ...jestBase
+  ...jestBase,
+  displayName: name,
+  name,
 }
 
 config.coverageThreshold['./src/server'] = {
   branches: 50,
   functions: 83,
   lines: 82.35,
-  statements: 83
+  statements: 83,
 }
 
-module.exports=config
+module.exports = config
