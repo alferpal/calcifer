8b7dabce74923c8370c4eb9437415b9233fa96a9
Alexandre Ferrando <alferpal@gmail.com>
2020-12-18 11:32:23 +0100

    feat(policies): add policies package

---
+++ packages/calcifer-policies/jest.config.js
@@ -0,0 +1,11 @@
+
+const { name } = require('./package')
+const jestBase = require('../../jest.config.js')
+
+const config = {
+  ...jestBase,
+  displayName: name,
+  name,
+}
+
+module.exports = config
