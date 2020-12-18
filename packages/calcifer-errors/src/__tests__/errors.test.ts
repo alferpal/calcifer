d824589fccba3697f0cf385ae17ba7745fb98b0e
Alex Ferrando <alferpal@gmail.com>
2020-12-18 11:32:07 +0100

    feat(calcifer-errors): add errors package

---
+++ packages/calcifer-errors/src/__tests__/errors.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, it } from '@jest/globals'
+import * as errors from '../index'
+
+describe('when errors is required', () => {
+  it('errors should be an object', () => {
+    expect.assertions(3)
+
+    expect(errors).toBeDefined()
+    expect(typeof errors).toStrictEqual('object')
+    expect(Object.keys(errors).length).toBeGreaterThan(0)
+  })
+})
+
+describe('errors', () => {
+  // eslint-disable-next-line jest/prefer-expect-assertions
+  it('all errors should be instantiable and have name and code properties', async () => {
+    Object.values(errors).forEach((ErrorClass) => {
+      const error = new ErrorClass('testMessage')
+
+      expect(error).toBeDefined()
+      expect(error).not.toBeNull()
+
+      expect(error).toBeInstanceOf(Error)
+
+      expect(error.name).toStrictEqual(ErrorClass.name)
+
+      expect(error.code).toStrictEqual(ErrorClass.ERROR_CODE)
+
+      // console.dir(error)
+    })
+  })
+})
---
+++ packages/calcifer-errors/src/index.ts
@@ -0,0 +1,15 @@
+import { BadDataError } from './errors/bad-data-error'
+import { ForbiddenError } from './errors/forbidden-error'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { NotFoundError } from './errors/not-found-error'
+import { ServerError } from './errors/server-error'
+import { UserNotFoundError } from './errors/user-not-found-error'
+
+export {
+  BadDataError,
+  ForbiddenError,
+  InvalidCredentialsError,
+  NotFoundError,
+  ServerError,
+  UserNotFoundError,
+}
