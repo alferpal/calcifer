5bd1f4e097fdf8fe3fd9f6dd9115e936bd5f87aa
Alexandre Ferrando <alferpal@gmail.com>
2020-12-20 21:55:25 +0100

    feat(utils): add http client utility

--- packages/calcifer-utils/src/index.ts
+++ packages/calcifer-utils/src/index.ts
@@ -1,5 +1,8 @@
+import { getClient } from './lib/http-client'
 import { hasRole } from './lib/has-role'
 import { logger } from './lib/log'
 import { setProcessDefaults } from './lib/process-defaults'
 
-export { hasRole, logger, setProcessDefaults }
+export {
+  getClient, hasRole, logger, setProcessDefaults,
+}
