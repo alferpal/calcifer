473ab14efd13f96db72f10257ccd821a52aa4791
Alexandre Ferrando <alferpal@gmail.com>
2021-01-11 16:28:09 +0100

    docs(calcifer-utils): add missing docs

--- packages/calcifer-utils/src/lib/has-role.ts
+++ packages/calcifer-utils/src/lib/has-role.ts
@@ -1,5 +1,12 @@
 import * as CalciferTypes from '@alferpal/calcifer-types'
 
+/**
+ * Checks wether a user has a role, returning true or false as appropiate
+ * Always returns true for admin users
+ * @param user The UserAuth interface instance to check
+ * @param role The role to check
+ * @returns boolean
+ */
 function hasRole(user: CalciferTypes.User.UserAuth, role: string) {
   const { roles = [] } = user
 
--- packages/calcifer-utils/src/lib/http-client.ts
+++ packages/calcifer-utils/src/lib/http-client.ts
@@ -1,8 +1,14 @@
-import got, { ExtendOptions } from 'got'
+import got, { ExtendOptions, Got } from 'got'
 import hyperid from 'hyperid'
 
 const idGenerator = hyperid()
 
+/**
+ * Handler for unhandledRejection
+ * Prints the error synchronously and exits the process
+ * @param options got options used for creating the client
+ * @returns A got instance
+ */
 function getClient(options: ExtendOptions = {}) {
   return got.extend(got.mergeOptions(options, {
     hooks: {
