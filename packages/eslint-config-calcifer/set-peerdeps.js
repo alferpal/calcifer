0d0a9d2da0ba41f7ac7ad1b3588fc36f6e40d1a2
Alex Ferrando <alferpal@gmail.com>
2021-02-07 09:48:40 +0100

    chore(eslint-config-calcifer): add new line after writing package.json

--- packages/eslint-config-calcifer/set-peerdeps.js
+++ packages/eslint-config-calcifer/set-peerdeps.js
@@ -3,4 +3,4 @@ const pkg = require('./package.json')
 
 pkg.peerDependencies = pkg.devDependencies
 
-writeFileSync('./package.json', JSON.stringify(pkg, null, 2), { encoding: 'utf-8' })
+writeFileSync('./package.json', `${JSON.stringify(pkg, null, 2)}\n`, { encoding: 'utf-8' })
